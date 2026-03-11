8ba3b79702481c3a3d7cce839402da8364d2ddcb
FodorRobert <fodorr@kkszki.hu>
2026-03-11 08:52:40 +0100

    Add project files.

---
+++ WPFdoga_FR_12B/datas/Show.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFdoga_FR_12B.datas
+{
+    internal class Show
+    {
+    }
+}
